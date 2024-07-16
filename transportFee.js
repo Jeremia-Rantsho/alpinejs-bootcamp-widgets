900c66dd41b77e95cc6136b9620e0617c0f8adb6
jeremia-rantsho <rantshodiketso17@gmail.com>
2024-07-16 13:43:47 +0200

    Added the transport fee functionality

---
+++ transportFee.js
@@ -0,0 +1,23 @@
+
+function transportFeeCalculator() {
+    return {
+        selectedShift: 'morning',
+        fee: '',
+
+        calculateFee() {
+            switch (this.selectedShift) {
+                case 'morning':
+                    this.fee = 'R20';
+                    break;
+                case 'afternoon':
+                    this.fee = 'R10';
+                    break;
+                case 'nightshift':
+                    this.fee = 'Free';
+                    break;
+                default:
+                    this.fee = 'Invalid shift';
+            }
+        }
+    };
+}
